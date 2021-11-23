2a247870ff1f2b9f57cd2d42eae8cc7f77942ee4
Konstantina Koliopoulou <sdi1100036@gmail.com>
2021-11-24 01:31:56 +0200

    refactor: -splits components

---
+++ components/requestCard.js
@@ -0,0 +1,86 @@
+import { CardActions, CardContent } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { toEther } from '../functions/web3Funcs';
+import Button from '@mui/material/Button';
+import * as React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { updateVision } from '../functions/updateVision';
+import { getReasonMessage } from '../functions/getReasonMessage';
+import useVisionContract from '../hooks/useVisionContract';
+import { useWeb3React } from '@web3-react/core';
+import useAlert from '../hooks/useAlert';
+import { Web3Context } from '../pages';
+import PropTypes from 'prop-types';
+
+const RequestCard = ({ req, idx, vision, setVision, isInvestor }) => {
+  const { web3 } = useContext(Web3Context);
+  const visionContract = useVisionContract(vision.visionAddress, web3);
+  const { account } = useWeb3React();
+  const { addAlert } = useAlert();
+  const [userHasVoted, setUserHasVoted] = useState(false);
+
+  useEffect(async () => {
+    let hasUserAlreadyVoted = await visionContract.methods.hasUserVoted(idx, account).call();
+    setUserHasVoted(hasUserAlreadyVoted);
+  }, []);
+
+  const vote = async (idx) => {
+    visionContract.methods.vote(idx).send({ from: account }).then((resp) => {
+      addAlert('Voted successfully!', 'success');
+      updateVision(web3, vision, setVision);
+      setUserHasVoted(true);
+    }).catch((error) => {
+      let reasonMessage = getReasonMessage(error);
+      reasonMessage = reasonMessage !== null ? reasonMessage.toString() : 'An error occured during voting';
+      addAlert(reasonMessage, 'error');
+    });
+  };
+
+  const withdraw = async (idx) => {
+    await visionContract.methods.withdraw(idx).send({ from: account }).then((resp) => {
+      addAlert('Amount successfully withdrawn!', 'success');
+      updateVision(web3, vision, setVision);
+    }).catch((error) => {
+      let reasonMessage = getReasonMessage(error);
+      reasonMessage = reasonMessage !== null ? reasonMessage.toString() : 'An error occured during withdrawal';
+      addAlert(reasonMessage, 'error');
+    });
+  };
+
+  const canWithdraw = (req) => req.votes >= vision._sumOfInvestors / 2 && !(req.votes !== 1 && vision._sumOfInvestors === 1);
+
+  return (
+    <CardContent>
+      <Typography variant='body2'>
+        Withdraw Amount: {toEther(req.withdrawAmount)} Withdraw
+        Reason: {req.withdrawalReason}
+      </Typography>
+      <Typography variant='body2'>
+        Num of Votes: {req.votes} Total
+        Investor:{vision._sumOfInvestors} Status: {req.isDone ? 'Completed' : 'Pending'}
+      </Typography>
+      <CardActions>
+        <Button size='small'
+                disabled={req.isDone || vision._owner === account || !isInvestor || userHasVoted}
+                onClick={() => {
+                  vote(idx);
+                }}>{userHasVoted ? 'Already Voted' : 'Vote'}</Button>
+        <Button size='small'
+                disabled={req.isDone || vision._owner !== account || !canWithdraw(req)}
+                onClick={() => {
+                  withdraw(idx);
+                }}>Withdraw</Button>
+      </CardActions>
+    </CardContent>
+  );
+};
+
+RequestCard.propTypes = {
+  vision: PropTypes.oneOfType([PropTypes.object]),
+  setVision: PropTypes.func,
+  req: PropTypes.oneOfType([PropTypes.object]),
+  idx: PropTypes.number,
+  isInvestor: PropTypes.bool,
+};
+
+export default RequestCard;
--- components/voteModal.js
+++ components/voteModal.js
@@ -1,22 +1,15 @@
 import * as React from 'react';
 import { useContext, useEffect, useState } from 'react';
-import { useWeb3React } from '@web3-react/core';
 import Button from '@mui/material/Button';
 import { Dialog, DialogTitle } from '@material-ui/core';
 import { Card, CardActions, CardContent, Grid } from '@mui/material';
-import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import { Web3Context } from '../pages';
-import { toEther } from '../functions/web3Funcs';
-import { updateVision } from '../functions/updateVision';
-import useAlert from '../hooks/useAlert';
-import { getReasonMessage } from '../functions/getReasonMessage';
 import useVisionContract from '../hooks/useVisionContract';
+import RequestCard from './requestCard';
 
 const VoteModal = ({ vision, setVision, isInvestor }) => {
-  const { web3, contract } = useContext(Web3Context);
-  const { account } = useWeb3React();
-  const { addAlert } = useAlert();
+  const { web3 } = useContext(Web3Context);
   const [requests, setRequests] = useState(null);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -29,35 +22,6 @@ const VoteModal = ({ vision, setVision, isInvestor }) => {
     setRequests(withdrawReqs);
 
   }, [vision]);
-
-  const vote = async (idx) => {
-    visionContract.methods.vote(idx).send({ from: account }).then((resp) => {
-      addAlert('Voted successfully!', 'success');
-      updateVision(web3, vision, setVision);
-    }).catch((error) => {
-      let reasonMessage = getReasonMessage(error);
-      reasonMessage = reasonMessage !== null ? reasonMessage.toString() : 'An error occured during voting';
-      addAlert(reasonMessage, 'error');
-    });
-  };
-
-  const withdraw = async (idx) => {
-    await visionContract.methods.withdraw(idx).send({ from: account }).then((resp) => {
-      addAlert('Amount successfully withdrawn!', 'success');
-      updateVision(web3, vision, setVision);
-    }).catch((error) => {
-      let reasonMessage = getReasonMessage(error);
-      reasonMessage = reasonMessage !== null ? reasonMessage.toString() : 'An error occured during withdrawal';
-      addAlert(reasonMessage, 'error');
-    });
-  };
-
-  const canWithdraw = (req) => req.votes >= vision._sumOfInvestors / 2 && !(req.votes !== 1 && vision._sumOfInvestors === 1);
-
-  const cannotVote = async (req, idx) => {
-    let hasUserAlreadyVoted = await visionContract.methods.hasUserVoted(idx, account).call();
-    return req.isDone || vision._owner === account || !isInvestor || hasUserAlreadyVoted;
-  };
 
   return (
     <>
@@ -73,28 +37,8 @@ const VoteModal = ({ vision, setVision, isInvestor }) => {
 
             {requests &&
             requests.map((req, idx) => (
-              <CardContent key={`req-${idx}`}>
-                <Typography variant='body2'>
-                  Withdraw Amount: {toEther(req.withdrawAmount)} Withdraw
-                  Reason: {req.withdrawalReason}
-                </Typography>
-                <Typography variant='body2'>
-                  Num of Votes: {req.votes} Total
-                  Investor:{vision._sumOfInvestors} Status: {req.isDone ? 'Completed' : 'Pending'}
-                </Typography>
-                <CardActions>
-                  <Button size='small'
-                          disabled={cannotVote(req, idx)}
-                          onClick={() => {
-                            vote(idx);
-                          }}>Vote</Button>
-                  <Button size='small'
-                          disabled={req.isDone || vision._owner !== account || !canWithdraw(req)}
-                          onClick={() => {
-                            withdraw(idx);
-                          }}>Withdraw</Button>
-                </CardActions>
-              </CardContent>
+              <RequestCard key={`${idx}-req-card`} isInvestor={isInvestor} vision={vision} setVision={setVision}
+                           idx={idx} req={req} />
             ))
             }
             {requests === null || requests.length === 0 && <CardContent>No requests to show</CardContent>}
